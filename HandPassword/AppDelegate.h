3e6908c8d1bfd24a12866bb1481950cd62b5e894
Dongdong <XUDONGDONG121@pingan.com.cn>
2016-04-14 16:15:55 +0800

    Initial Commit

---
+++ HandPassword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandPassword
+//
+//  Created by Dongdong on 16/4/14.
+//  Copyright © 2016年 Dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HandPassword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandPassword
+//
+//  Created by Dongdong on 16/4/14.
+//  Copyright © 2016年 Dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
